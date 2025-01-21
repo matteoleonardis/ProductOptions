fa011bbe1b8a0d6f3ab97b0e30cdeab081bfe906
matteoleonardis <16738037+matteoleonardis@users.noreply.github.com>
2025-01-21 14:38:55 +0100

    Create requirejs-config.js

---
+++ base/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_PageBuilder/js/content-type/products/appearance/carousel/widget': {
+                'matteoleonardis_ProductOptions/js/content-type/products/appearance/carousel/widget-mixin': true
+            }
+        }
+    }
+};
